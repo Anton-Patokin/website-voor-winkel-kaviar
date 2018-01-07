07f459cb63acecbdc588518f28adce468d7854e7
Anton-Patokin <patokin.antonrgmail.com>
2018-01-07 22:02:05 +0100

    minify and 404 page

---
+++ Kaviar/resources/views/errors/404.blade.php
@@ -0,0 +1,21 @@
+<style>
+    .logo h1,.logo p{text-align:center}body{font-family:Courgette,cursive;background:#fff}.wrap{margin:0 auto;width:1000px}.logo{margin-top:50px}.logo h1{font-size:200px;color:#2c3e50;margin-bottom:1px;text-shadow:1px 1px 6px #fff}.logo p{color:#e492a2;font-size:20px;margin-top:1px}.logo p span{color:#90ee90}
+    }	</style>
+<html>
+<head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Error 404</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="//fonts.googleapis.com/css?family=Courgette" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div class="wrap">
+    <div class="logo">
+        <h1>404</h1>
+        <p>Error occurred! - File not Found</p>
+        <div class="sub">
+            <p><a href="#">Back</a></p>
+        </div>
+    </div>
+</div>
+</body>
+</html>
